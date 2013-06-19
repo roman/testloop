8aa4da9b7b7a0d04a31baae6a2e7f127983eb9bd
Roman Gonzalez <romanandreg@gmail.com>
2013-06-18 19:00:26 -0700

    Bumping to v0.1.0.1 (laxing dependencies on base)

--- examples/testloop-example/startTestLoop.sh
+++ examples/testloop-example/startTestLoop.sh
@@ -2,9 +2,10 @@
 
 function _runTestLoop {
     cabal-dev add-source ../../../testloop
-    cabal-dev install testloop --reinstall
+    cabal-dev configure --enable-test
+    cabal-dev install --only-dependencies
+    # cabal-dev install testloop --reinstall
     cabal-dev clean
-    cabal-dev configure --enable-tests
     cabal-dev build
     echo -e "\x1B[32mTestLoop Ready\x1B[0m"
     echo -e "Go to \x1B[33mtest/TestSuite.hs\x1B[0m, modify the file and see things running automatically"
